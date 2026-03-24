ca5a07f9979f6dff59b63a601eb607b4107e4284
pietrofofo <143614391+pietrofofo@users.noreply.github.com>
2026-03-24 11:40:04 -0300

    Add files via upload

---
+++ interpolação.js
@@ -0,0 +1,11 @@
+const nome = "Pietro";
+const idade = "15";
+const anoAtual = "2026";
+
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10;
+
+console.log("O ano de nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log(`Daqui há 10 anos, terá ${idadeEmDezAnos} anos.`);
+
