aead8c8073785261fdcd71a4442a14bb73397b7d
pietrofofo <143614391+pietrofofo@users.noreply.github.com>
2026-04-15 13:44:24 +0000

    V1

---
+++ lista.js
@@ -0,0 +1,8 @@
+const alunos = ["Alice", "Bruna", "Carla"];
+
+alunos.push("Diego");
+alunos.unshift("Lopes");
+console.log(alunos);
+
+console.log(alunos.indexOf("Diego"));//Retorna o índice do array
+console.log(alunos.indexOf("Paula"));
