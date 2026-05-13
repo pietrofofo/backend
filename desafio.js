f72886a260c129049a3db7de08666b53d97ef1a2
pietrofofo <143614391+pietrofofo@users.noreply.github.com>
2026-05-13 13:53:07 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,16 @@
+//parte1
+const nomeProduto = "notebook gamer"; 
+let preco = 4500.99; 
+const sku = "acernitroV15"; 
+let quantidadeEstoque = 1; 
+let disponivel = true; 
+
+console.log("Produto: " + nomeProduto + ", Preço: r$ " + preco);
+
+//parte2
+let numero1 = 10;
+let numero2 = 7;
+
+let resultado = numero1 - numero2;
+
+console.log(numero1 + " - " + numero2 + " = " + resultado);
